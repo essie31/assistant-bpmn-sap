4106a435e12e959c5d883a4bee7d6995fdf4c884
ihsane <ihsanelaklache7@gmail.com>
2026-04-01 15:08:04 +0100

    Update lavasser.py

--- lavasser.py
+++ lavasser.py
@@ -55,7 +55,7 @@ if check_password():
     # ==========================================
     GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
     client = Groq(api_key=GROQ_API_KEY)
-    MODEL_NAME = "llama-3.3-70b-versatile"
+    OMODEL_NAME = "llama-3.1-8b-instant"
 
     PILIERS = {
         1: "Big Data / Analytics", 2: "Robots Autonomes", 3: "Simulation",
